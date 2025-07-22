ae2145beb1d1e27128b74fc714b75923505afebc
Tenzin Kalsang <109454940+fawfol@users.noreply.github.com>
2025-07-22 15:44:48 +0530

    Add files via upload

---
+++ src/screens/Auth/AuthLoadingScreen.js
@@ -0,0 +1,36 @@
+//Duogram/src/screens/Auth/AuthLoadingScreen.js
+import React, { useEffect } from 'react';
+import { ActivityIndicator, View } from 'react-native';
+import { auth, db } from '../../firebase';
+import { doc, getDoc } from 'firebase/firestore';
+
+export default function AuthLoadingScreen({ navigation }) {
+  useEffect(() => {
+  const unsubscribe = auth.onAuthStateChanged(async (user) => {
+    try {
+      if (user) {
+        const userDoc = await getDoc(doc(db, "users", user.uid));
+        if (userDoc.exists() && userDoc.data().pairCode) {
+          navigation.replace('Main');
+        } else {
+          navigation.replace('Pair');
+        }
+      } else {
+        navigation.replace('Login');
+      }
+    } catch (error) {
+      console.error("Auth check failed:", error);
+      navigation.replace('Login'); // fallback
+    }
+  });
+
+  return unsubscribe;
+}, []);
+
+
+  return (
+    <View style={{flex:1, justifyContent:'center', alignItems:'center'}}>
+      <ActivityIndicator size="large" color="#007AFF" />
+    </View>
+  );
+}
